221ea59564964ee7131391d100928c5542adbfc1
Ben <ben@alt-design.net>
2023-12-04 10:25:03 +0000

    Hotfix

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::group(['middleware' => ['statamic.cp.authenticated'], 'namespace' => 'AltDesign\AltSitemap\Http\Controllers'], function() {
+Route::group(['namespace' => 'AltDesign\AltSitemap\Http\Controllers'], function() {
     Route::get('/sitemap.xml', 'AltSitemapController@generateSitemap');
 });
