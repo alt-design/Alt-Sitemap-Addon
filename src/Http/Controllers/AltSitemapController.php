d4973cdf93f94081d6a6689e7df2223f1203a199
Richard Bagshaw <richard@bagshaw.co.uk>
2025-02-03 14:06:08 +0000

    WIP

--- src/Http/Controllers/AltSitemapController.php
+++ src/Http/Controllers/AltSitemapController.php
@@ -1,10 +1,15 @@
-<?php namespace AltDesign\AltSitemap\Http\Controllers;
+<?php 
+
+declare(strict_types=1);
+
+namespace AltDesign\AltSitemap\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
 use Statamic\Facades\Entry;
 use AltDesign\AltSitemap\Helpers\Data;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 use Statamic\Facades\Term;
 
 class AltSitemapController
@@ -65,6 +70,175 @@ class AltSitemapController
     }
 
     public function generateSitemap(Request $request)
+    {
+        if (config('statamic.eloquent-driver.entries.driver') == 'eloquent') {
+            return $this->generateEloquentSitemap($request);
+        } else {
+            return $this->generateFlatFileSitemap();
+        }
+    }
+
+    private function generateEloquentSitemap(Request $request)
+    {
+        //get blueprint setting values
+        $data = new Data('settings');
+        $blueprint = $data->getBlueprint(true);
+        $fields = $blueprint->fields()->addValues($data->all())->preProcess();
+        $defaultCollectionPriorities = $fields->values()->toArray()['default_collection_priorities'];
+        $excludeCollectionFromSitemap = $fields->values()->toArray()['exclude_collections_from_sitemap'];
+        $defaultTaxonomyPriorities = $fields->values()->toArray()['default_taxonomy_priorities'];
+        $excludeTaxonomiesFromSitemap = $fields->values()->toArray()['exclude_taxonomies_from_sitemap'];
+
+        foreach ($defaultCollectionPriorities as $value) {
+            $collection = $value['collection'][0];
+            $priority = $value['priority'];
+            $settings[] = array($collection, $priority) ;
+        }
+
+         foreach ($defaultTaxonomyPriorities as $value) {
+             $taxonomy = $value['taxonomy'][0];
+             $priority = $value['priority'];
+             $settings[] = array($taxonomy, $priority) ;
+         }
+
+        // $site_url = url('');
+        $site_url = $request->getSchemeAndHttpHost();
+        // $entries = Entry::all();
+
+        $items = [];
+
+        $entries = DB::table("entries")
+            ->select(
+                "id",
+                "uri",
+                "data->title as title",
+                "data->sitemap_priority as sitemap_priority",
+                "data->exclude_from_sitemap as exclude_from_sitemap",
+                "collection",
+                "updated_at",
+                "published"
+            )
+            ->get()
+            ->map(function ($entry) {
+                return [
+                    'id' => $entry->id,
+                    'title' => $entry->title,
+                    'uri' => $entry->uri,
+                    'sitemap_priority' => (int) $entry->sitemap_priority,
+                    'exclude_from_sitemap' => $entry->exclude_from_sitemap === 'true' ? true : false,
+                    'collection' => $entry->collection,
+                    'published' => $entry->published,
+                    'updated_at' => Carbon::make($entry->updated_at)->format('Y-m-d\TH:i:sP'),
+                ];
+            });
+
+        foreach ($entries as $entry) {
+            $url = $entry['uri'];
+            // Skip if the entry is not published
+            if ($entry['published'] === false) {
+                continue;
+            }
+
+            // skip if to be excluded
+            if ($entry['exclude_from_sitemap'] === true) {
+                continue;
+            }
+
+            // skip if collection is to be excluded
+            if (in_array($entry['collection'], $excludeCollectionFromSitemap)) {
+                continue;
+            }
+
+            // if the collection has no route setup, skip
+            if ($url === null) {
+                continue;
+            }
+
+            //check if entry collection matches setting[0], if so apply setting[1] as priority
+            $priority = 0.5;
+            // $entryCollection = $entry->collection->handle;
+            $entryCollection = $entry['collection'];
+
+            foreach ($settings ?? [] as $setting) {
+                if ($entryCollection == $setting[0]) {
+                    $priority = $setting[1];
+                }
+            }
+
+            // override with priority from entry if set
+            $priority = $entry['sitemap_priority'] ?? $priority;
+            $items[] = array($url, $entry['updated_at'], $priority);
+        }
+
+        // Add terms to sitemap
+        $terms = DB::table("taxonomy_terms")
+            ->select(
+                "id",
+                "site",
+                "slug",
+                "uri",
+                "taxonomy",
+                "created_at",
+                "updated_at",
+            )
+            ->get()
+            ->map(function ($entry) {
+                return [
+                    'id' => $entry->id,
+                    'site' => $entry->site,
+                    'slug' => $entry->slug,
+                    'uri' => $entry->uri,
+                    'taxonomy' => $entry->taxonomy,
+                    'created_at' => Carbon::make($entry->created_at)->format('Y-m-d\TH:i:sP'),
+                    'updated_at' => Carbon::make($entry->updated_at)->format('Y-m-d\TH:i:sP'),
+                ];
+            });
+
+        foreach ($terms as $term) {
+            $url = $term['uri'];
+            // skip if term is to be excluded
+//            if ($term->exclude_from_sitemap == true) {
+//                continue;
+//            }
+//
+//            // skip if taxonomy is to be excluded
+//            if (in_array($term->taxonomy->handle, $excludeTaxonomiesFromSitemap)) {
+//                continue;
+//            }
+
+            // check if term taxonomy matches setting[0], if so apply setting[1] as priority
+            $priority = 0.5;
+            $termTaxonomy = $term['taxonomy'];
+            foreach ($settings ?? [] as $setting) {
+                if ($termTaxonomy == $setting[0]) {
+                    $priority = $setting[1];
+                }
+            }
+            // override with priority from entry if set
+            $priority = $term->sitemap_priority ?? $priority;
+            $items[] = array($url, $term['updated_at'], $priority);
+        }
+
+         foreach ($this->manualItems as $manualItem) {
+             $url = $manualItem[0] ?? null;
+             if (empty($url)) {
+                 continue;
+             }
+             $lastModified = $manualItem[1]->format('c') ?? \Carbon\Carbon::now()->format('c');
+             $priority = $manualItem[2] ?? 0.5;
+             $items[] = [$url, $lastModified, $priority];
+         }
+
+        $xml = '<?xml version="1.0" encoding="UTF-8"?>';
+        $xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+        foreach ($items as $item) {
+            $xml .='<url><loc>'.$site_url.$item[0].'</loc><lastmod>'.$item[1].'</lastmod><priority>'.$item[2].'</priority></url>';
+        }
+        $xml .= '</urlset>';
+        return Response::make($xml, 200, ['Content-Type' => 'application/xml']);
+    }
+
+    private function generateFlatFileSitemap()
     {
         //get blueprint setting values
         $data = new Data('settings');
@@ -169,3 +343,4 @@ class AltSitemapController
         return Response::make($xml, 200, ['Content-Type' => 'application/xml']);
     }
 }
+
